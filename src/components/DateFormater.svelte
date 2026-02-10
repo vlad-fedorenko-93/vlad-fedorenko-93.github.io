37353068d4c4ffe90f43628679e74082aeb016e4
Vlad <gnatrusyy@gmail.com>
2026-02-10 11:42:03 +0200

    fixed build error

--- src/components/DateFormater.svelte
+++ src/components/DateFormater.svelte
@@ -1,9 +1,13 @@
 <script>
-    let { date, className } = $props();
+  let { date, className } = $props();
 </script>
 
-<p class={className}>{date.toLocaleDateString("en-UK", {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        })}</p>
+<p class={className}>
+  {date
+    ? date.toLocaleDateString("en-UK", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })
+    : ""}
+</p>
